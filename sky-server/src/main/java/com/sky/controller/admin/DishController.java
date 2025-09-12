eb1c83415941827fcc54ff2170fde5fc692759fb
Alex Jones <wangshihaoit@gmail.com>
2025-09-12 13:04:55 +0800

    新增菜品分页查询功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,16 +1,16 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -36,5 +36,18 @@ public class DishController {
 		log.info("新增菜品: {}", dishDTO);
 		dishService.saveWithFlavor(dishDTO);
 		return Result.success();
+	}
+
+	/**
+	 * 菜品分页查询
+	 * @param dishPageQueryDTO 分页查询参数
+	 * @return 分页结果
+	 */
+	@GetMapping("/page")
+	@Operation(summary = "菜品分页查询")
+	public Result<PageResult<DishVO>> pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+		log.info("菜品分页查询: {}", dishPageQueryDTO);
+		PageResult<DishVO> pageResult = dishService.pageQuery(dishPageQueryDTO);
+		return Result.success(pageResult);
 	}
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
 
 public interface DishService {
 
@@ -9,4 +12,11 @@ public interface DishService {
 	 * @param dishDTO 菜品信息
 	 */
 	void saveWithFlavor(DishDTO dishDTO);
+
+	/**
+	 * 菜品分页查询
+	 * @param dishPageQueryDTO 分页查询参数
+	 * @return 分页结果
+	 */
+	PageResult<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
